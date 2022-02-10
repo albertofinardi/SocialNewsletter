e52e09c8b4ed5f3e82c1c41370f6f89539a47194
Alberto Finardi <finardialberto@gmail.com>
2022-02-10 15:59:15 +0100

    Twitter API

--- src/app/twitter/core/twitter_manager.ts
+++ src/app/twitter/core/twitter_manager.ts
@@ -1,4 +1,4 @@
-import { TwitterApi } from 'twitter-api-v2';
+import { http } from "@tauri-apps/api";
 
 export class TweetQuery {
     bearer_token: string = '';
@@ -12,40 +12,109 @@ export class TweetQuery {
     backup_folder: string = '';
     export_csv_backup: string = '';
     export_json_backup: string = '';
-    /*constructor(bearer_token: string,
-        max_tweets: number,
-        min_likes: number,
-        max_added_or_fetched: string,
-        query: string,
-        days_span: number,
-        export_csv: string,
-        export_json: string,
-        backup_folder: string,
-        export_csv_backup: string,
-        export_json_backup: string) {
-        this.bearer_token = bearer_token,
-            this.max_tweets = max_tweets,
-            this.min_likes = min_likes,
-            this.max_added_or_fetched = max_added_or_fetched,
-            this.query = query,
-            this.days_span = days_span,
-            this.export_csv = export_csv,
-            this.export_json = export_json,
-            this.backup_folder = backup_folder,
-            this.export_csv_backup = export_csv_backup,
-            this.export_json_backup = export_json_backup
-    }*/
 }
 
-export async function start(twq: TweetQuery) {
-    const max_results = 100;
+async function fetchData(twq: TweetQuery, max_results: number, today: string, start: string, next_token: string | null): Promise<http.Response<any>> {
+
+    var headers = "Bearer " + twq.bearer_token
+    var url = "https://api.twitter.com/2/tweets/search/recent?query=" + twq.query + "&start_time=" + start + "&end_time=" + today + "&max_results=" + max_results.toString() + "&tweet.fields=id,text,author_id,created_at,public_metrics&expansions=author_id&user.fields=id,name,username,public_metrics";
+    if (next_token != null) {
+        url += "&next_token=" + next_token;
+    }
+    return http.fetch(encodeURI(url), {
+        method: "GET", headers: {
+            "Authorization": headers.toString(),
+        }
+    })
+
+}
+
+function sleep(milliseconds: number) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+export async function go(twq: TweetQuery) {
+
+    function filterLike(tweet: any) {
+        return tweet.public_metrics.like_count >= twq.min_likes;
+    }
+
+    const max_results = 11;
     const today = new Date(Date.now() - 30000)
     const start = new Date(new Date().setDate(today.getDate() - twq.days_span) - 30000)
-    var headers = {"Authorization": "Bearer " + twq.bearer_token}
-    const params = new URLSearchParams("query="+twq.query+"&start_time="+start.toISOString()+"&end_time="+today.toISOString()+"&max_results"+max_results+"&tweet.fields=id,text,author_id,created_at,public_metrics&expansions=author_id&user.fields=id,name,username,public_metrics")
-    const url = new URL("https://api.twitter.com/2/tweets/search/recent");
-    url.search = params.toString();
-    console.log(url);
-    const response = await fetch( url.toString(), {headers, mode: 'no-cors'} );
-    return await response.json();
+
+    var data = []
+    var users = []
+
+    var total_tweets = 0;
+    var total_tweets_added = 0;
+    var count = 0;
+    var flag = true;
+    var next_token = null
+    var result_count = 0;
+
+    while (flag) {
+        if (twq.max_added_or_fetched == 'fetched' && total_tweets >= twq.max_tweets) {
+            break;
+        } else if (twq.max_added_or_fetched == 'added' && total_tweets_added >= twq.max_tweets) {
+            break;
+        }
+        console.log("-------------------")
+        console.log("Token: ", next_token)
+        var res: any = await fetchData(twq, max_results, today.toISOString(), start.toISOString(), next_token);
+
+        /* Se ci sono errori nella richiesta esce prima */
+        if (res.ok == false) {
+            var texts = res.data.errors.map((el: any) => el.message);
+            throw new Error(texts);
+        }
+
+        result_count = res.data.meta.result_count;
+        var filtered = res.data.data.filter(filterLike)
+
+        data.push(filtered)
+        /* Rimuovere utenti già eslusi post filter like */
+        users.push(res.data.includes.users)
+
+        if (res.data.meta.next_token != null && res.data.meta.next_token != "") {
+            next_token = res.data.meta.next_token
+            console.log("Next Token: ", next_token)
+            if (result_count != null && result_count > 0 && next_token != null) {
+                console.log("Start Date: ", start)
+                //tmp_count = convertData(min_likes, json_response, export_csv, result_count)
+                count += result_count
+                total_tweets_added += res.data.data.length - filtered.length
+                total_tweets += result_count
+                console.log("Total # of Tweets fetched: ", total_tweets)
+                console.log("Total # of Tweets added: ", total_tweets_added)
+                console.log("-------------------")
+                sleep(5000)
+            }
+        } else {
+            if (result_count != null && result_count > 0) {
+                console.log("-------------------")
+                console.log("Start Date: ", start)
+                //tmp_count = convertData(min_likes, json_response, export_csv, result_count)
+                count += result_count
+                total_tweets_added += res.data.data.length - filtered.length
+                total_tweets += result_count
+                console.log("Total # of Tweets fetched: ", total_tweets)
+                console.log("Total # of Tweets added: ", total_tweets_added)
+                console.log("-------------------")
+                sleep(5000)
+            }
+            flag = false
+            next_token = ""
+        }
+        sleep(5000)
+
     }
+    console.log("Total number of fetched: ", total_tweets)
+    console.log("Total number of added: ", total_tweets_added)
+    console.log(data)
+    console.log(users)
+}
--- src/app/twitter/twitter.component.ts
+++ src/app/twitter/twitter.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { dialog, fs } from '@tauri-apps/api';
 
-import { TweetQuery, start } from './core/twitter_manager';
+import { TweetQuery, go } from './core/twitter_manager';
 
 
 @Component({
@@ -9,14 +10,16 @@ import { TweetQuery, start } from './core/twitter_manager';
   templateUrl: './twitter.component.html',
   styleUrls: ['./twitter.component.css']
 })
-export class TwitterComponent implements OnInit {
+export class TwitterComponent {
+
+  error = ''
 
   twitterSettings = new FormGroup({
-    bearer_token: new FormControl('', Validators.required),
+    bearer_token: new FormControl('AAAAAAAAAAAAAAAAAAAAALO0YQEAAAAA0DVeZeNa0gvvIMyz1oupExmLSwY%3DkueA2JTVEFh44CwPG5Xfu7grxCx0G7MPMYNWkugbHVn7Ts6NnI', Validators.required),
     max_tweets: new FormControl(500),
     min_likes: new FormControl(0, Validators.required),
     max_added_or_fetched: new FormControl('fetched'),
-    query: new FormControl(''),
+    query: new FormControl('a'),
     days_span: new FormControl(1, Validators.required),
     export_csv: new FormControl('data', Validators.required),
     export_json: new FormControl('data', Validators.required),
@@ -25,22 +28,36 @@ export class TwitterComponent implements OnInit {
     export_json_backup: new FormControl('data_backup'),
   });
 
-  async onSubmit(){
+  async onSubmit() {
     let tweetQuery = new TweetQuery()
     tweetQuery = this.twitterSettings.value;
-    const res = await start(tweetQuery);
-    console.log(res);
+    this.error = 'Loading...'
+    try {
+      await go(tweetQuery);
+    } catch (err: any) {
+      this.error = err
+    }
+    /*startA(tweetQuery).then((res: any) => {
+      if (res.ok == false) {
+        this.error = res.data.detail.toString()
+      } else {
+        this.error = ''
+      }
+      console.log(res)
+    }).catch(err => {
+      this.error = err
+    })*/
   }
-/*
-  async loadProfile(){
+
+  async loadProfile() {
     let file;
     dialog.open().then(async path => {
       file = await fs.readTextFile(path.toString())
       let json = JSON.parse(file)
-      
+
       Object.keys(this.twitterSettings.controls).forEach(key => {
         this.twitterSettings.controls[key].setValue(json[key])
-        if(key == 'max_tweets' || key == 'min_likes' || key == 'days_span'){
+        if (key == 'max_tweets' || key == 'min_likes' || key == 'days_span') {
           this.twitterSettings.controls[key].setValue(parseInt(json[key]))
         }
 
@@ -49,10 +66,10 @@ export class TwitterComponent implements OnInit {
 
   }
 
-  async saveProfile(){
+  async saveProfile() {
     let profile = JSON.stringify(this.twitterSettings.getRawValue());
     dialog.save({
-      filters: [{extensions: ['social'], name: 'social'}],
+      filters: [{ extensions: ['social'], name: 'social' }],
     }).then(path => {
       fs.writeFile({
         path: path,
@@ -61,7 +78,7 @@ export class TwitterComponent implements OnInit {
     })
   }
 
-  async backupFolder(){
+  async backupFolder() {
     dialog.open({
       directory: true,
       multiple: false,
@@ -69,12 +86,9 @@ export class TwitterComponent implements OnInit {
       this.twitterSettings.controls['backup_folder'].setValue(path)
     })
   }
-*/
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
-  
 
 }
